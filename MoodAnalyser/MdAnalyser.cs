162f0c4dd2fcb57ecad3b94dc5c1633979120c22
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-18 17:13:29 +0530

    Add project files.

---
+++ MoodAnalyser/MdAnalyser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    class MdAnalyser
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Welcome to mood analyser problem");
+        }
+    }
+}
